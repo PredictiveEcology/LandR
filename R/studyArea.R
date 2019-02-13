39e404d9bd67918d6de0787a81c88d7298229354
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-12 17:12:16 -0700

    add prepGADM() to easily get e.g., canada provincial boundaries

--- R/studyArea.R
+++ R/studyArea.R
@@ -5,3 +5,19 @@
 #' @export
 #' @importFrom utils getFromNamespace
 randomStudyArea <- getFromNamespace("randomStudyArea", "SpaDES.tools")
+
+#' Get and prep GADM maps
+#'
+#' A wrapper around \code{\link[raster]{getData}} that does additional cleanup and reprojects.
+#'
+#' @param country Character string giving the country code (default \code{"CAN"}).
+#' @param level   Numeric giving the ADM level to use (default \code{1}).
+#' @param prj     Character string giving the projection to use.
+#' @param dPath   The destination path in which to save the data file.
+#'
+#' @export
+prepGADM <- function(country = "CAN", level = 1, prj, dPath) {
+  getData("GADM", country = country, level = level, path = dPath) %>%
+    spTransform(mod$prj) %>%
+    fixErrors(objectName = paste0("GDAM_", country, level))
+}
