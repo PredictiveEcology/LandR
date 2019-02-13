4c7543aa2aecf7de7b778afef6536dc7937586cf
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-13 01:04:40 -0700

    remove prepGADM
    this is better handled using prepInputs directly

--- R/studyArea.R
+++ R/studyArea.R
@@ -5,23 +5,3 @@
 #' @export
 #' @importFrom utils getFromNamespace
 randomStudyArea <- getFromNamespace("randomStudyArea", "SpaDES.tools")
-
-#' Get and prep GADM maps
-#'
-#' A wrapper around \code{\link[raster]{getData}} that does additional cleanup and reprojects.
-#'
-#' @param country Character string giving the country code (default \code{"CAN"}).
-#' @param level   Numeric giving the ADM level to use (default \code{1}).
-#' @param proj     Character string giving the projection to use.
-#' @param dPath   The destination path in which to save the data file.
-#'
-#' @export
-#' @importFrom magrittr %>%
-#' @importFrom raster getData
-#' @importFrom reproducible fixErrors
-#' @importFrom sp spTransform
-prepGADM <- function(country = "CAN", level = 1, proj, dPath) {
-  getData("GADM", country = country, level = level, path = dPath) %>%
-    spTransform(proj) %>%
-    fixErrors(objectName = paste0("GDAM_", country, level))
-}
