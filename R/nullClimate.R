c28a58c4575a95c3602999f953fb71692c253cdf
Unknown <ianmseddy@gmail.com>
2019-03-13 13:38:00 -0700

    null climate functions

---
+++ R/nullClimate.R
@@ -0,0 +1,20 @@
+#'  Predict biomass change with climate variables
+#'  @return NULL in place of a model object
+calculateClimateEffect <- function(...){
+
+  return(NULL)
+}
+
+#' calculateClimateGrowth
+#' @return null estimate of growth due to climate
+#' @export
+calculateClimateGrowth <- function(...) {
+  0
+}
+
+#' calculateClimateMortality
+#' @return null estimate of mortality due to climate
+#' @export
+calculateClimateMortality <- function(...) {
+  0
+}
