41e19583934e713f05dd851ff421461b158d156e
Alex Chubaty <alex.chubaty@gmail.com>
2019-03-26 13:01:59 -0600

    Update nullClimate.R

--- R/nullClimate.R
+++ R/nullClimate.R
@@ -1,18 +1,20 @@
-#' calculateClimateEffect
+#' \code{calculateClimateEffect}
 #'
 #' @param ... additional arguments that are passed to LandR.CS
 #' @return NULL in place of a model object
+#'
 #' @export
 calculateClimateEffect <- function(...) {
   return(NULL)
 }
 
-#' assignClimateEffect
+#' \code{assignClimateEffect}
 #'
 #' @param ... additional arguments that are passed to LandR.CS
-#' @return null estimate of growth due to climate
+#' @return null (zero) estimate of growth due to climate
+#'
 #' @export
 assignClimateEffect <- function(...) {
-  0
+  return(0)
 }
 
