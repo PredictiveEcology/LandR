d6d67e0cb764f0c60fb060d1d9237b5b48aa5265
Alex Chubaty <alex.chubaty@gmail.com>
2019-04-12 13:52:52 -0600

    more docs cleanup

--- R/overlayLCCs.R
+++ R/overlayLCCs.R
@@ -4,11 +4,12 @@ if (getRversion() >= "3.1.0") {
 
 #' Overlay different LCC data sources
 #'
-#' @param LCCs A named list or named \code{RasterStack} of layers whose content is Land Cover Class
+#' @param LCCs A named list or named \code{RasterStack} of layers whose content
+#'   is Land Cover Class.
 #' @param forestedList A named list of same length and names as \code{LCCs} indicating
 #'   which classes in each LCC raster are 'forested', either permanent or transient
-#' @param outputLayer A character string that matches one of the named elements in \code{LCCs}.
-#'   This will be the classification system returned.
+#' @param outputLayer A character string that matches one of the named elements
+#'   in \code{LCCs}. This will be the classification system returned.
 #' @param NAcondition A character string of a vectorized logical statement that
 #'   will be parsed within the \code{forestEquivalencies} table.
 #'   It should be a set of conditions with \code{== 0}, i.e., non-forested.
@@ -16,7 +17,7 @@ if (getRversion() >= "3.1.0") {
 #'   where \code{0} is the non-forested pixels based on converting LCCs and
 #'   \code{forestedList} to 1s and 0s.
 #' @param NNcondition A character string of a vectorized logical statement that
-#'   will be parsed within the "forestEquivalencies" table.
+#'   will be parsed within the \code{forestEquivalencies} table.
 #'   It should be a set of conditions with \code{== 0}, i.e., non-forested.
 #'   Examples:, e.g., \code{"LCC2005 == 0"} or \code{"CC == 0 | LCC2005 == 0"},
 #'   where \code{0} is the non-forested pixels based on converting LCCs and
