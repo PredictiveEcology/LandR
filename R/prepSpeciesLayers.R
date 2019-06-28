f6a0d9ff9a5333d3ea57526c294508085070db45
Eliot McIntire <eliotmcintire@gmail.com>
2019-06-27 20:20:37 -0700

    prepSpeciesLayers -- protect a sometimes fail on CCstack

--- R/prepSpeciesLayers.R
+++ R/prepSpeciesLayers.R
@@ -479,7 +479,11 @@ prepSpeciesLayers_ForestInventory <- function(destinationPath, outputPath,
   NA_ids <- which(is.na(ml$LandType[]) |  # outside of studyArea polygon
                     ml$LandType[] == 1)   # 1 is cities -- NA it here -- will be filled in with another veg layer if available (e.g. Pickell)
   message("  Setting NA, 1 in LandType to NA in speciesLayers in ForestInventory data")
-  CCstack[NA_ids] <- NA
+  aa <- try(CCstack[NA_ids] <- NA, silent = TRUE)
+  if (is(aa, "try-error")) { # not clear why the above line sometimes fails with Error in value[j, ] : incorrect number of dimensions
+    l <- unstack(CCstack)
+    CCstack <- lapply(l, function(x) {x[NA_ids] <- NA; x})
+  }
   names(CCstack) <- equivalentName(CCstackNames, sppEquiv, sppEquivCol)
 
   stack(CCstack)
