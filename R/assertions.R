3877d51724f5f9ed4c6a1017aeee7c9df343234b
CeresBarros <ceresvbarros@hotmail.com>
2019-09-20 16:28:21 -0700

    documentation/namespace fixes and rebuild

--- R/assertions.R
+++ R/assertions.R
@@ -8,9 +8,10 @@ if (getRversion() >= "3.1.0") {
 #'
 #' @param cohortData34to36 A \code{cohortData} \code{data.table} with only the
 #'                         pixels what were lcc 34:36
+#' @param rmZeroBiomassQuote  An expression to evaluate, in the form of \code{quote(B>0)},
+#'    used to select cohorts with biomass.
 #'
 #' @template cohortData
-#'
 #' @template doAssertion
 #'
 #' @export
