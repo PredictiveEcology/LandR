0d52aff85df1c8591b98e31a6290ced54ee595c5
CeresBarros <cbarros@mail.ubc.ca>
2020-04-26 17:42:26 -0700

    interpolate shade-related germination probs

--- R/misc.R
+++ R/misc.R
@@ -5,20 +5,79 @@ if (getRversion() >= "3.1.0") {
 #' Assign light probability
 #'
 #' @param sufficientLight a \code{data.table} containing probability of establishment, given a
-#' site's light conditions (\code{X0}-\code{X5}) for each level of a specie's shade tolerance (1-5).
+#' site's light conditions (\code{X0}-\code{X5}) for each level of a species' shade tolerance (1-5).
 #'
 #' @param newCohortData  a modified version of \code{cohortData} that contains new cohorts.
+#'
+#' @param interpolate Logical. Activates interpolation of probabilities of establishment between
+#'   any two values of shade tolerance in the sufficient light table, allowing species shade tolarance
+#'   trait values to take any decimal value between 1 and 5 (inclusively). If false, species shade tolerances
+#'   can only take integer values between 1 and 5  (inclusively).
 #'
 #' @return  \code{newCohortData} with a \code{lightProb} column
 #'
 #' @export
-assignLightProb <- function(sufficientLight, newCohortData) {
+assignLightProb <- function(sufficientLight, newCohortData, interpolate = TRUE) {
   ## for each line, get the survival probability from sufficientLight table note
   ## that sufficentLight is a table of survival probs for each tolerance level
   ## (row) by and shade level (column) siteShade + 2 is necessary to skip the
   ## first column
-  newCohortData[, lightProb := sufficientLight[cbind(shadetolerance, siteShade + 2)]]
+  if (interpolate) {
+    ## calculate floors/ceilings of shade tolerance and corresponding probs.
+    tempDT <- newCohortData[, list(shadetolerance = shadetolerance,
+                                   siteShade = siteShade,
+                                   lowShadetol = floor(shadetolerance),
+                                   highShadetol = ceiling(shadetolerance))]
+    tempDT[, `:=`(lowProb = sufficientLight[cbind(lowShadetol + 1, siteShade + 2)],
+                  highProb = sufficientLight[cbind(highShadetol + 1, siteShade + 2)])]
+
+    ## interpolate between floor/ceiling to find prob, then add to newCohortData
+    tempDT[, lightProb := .interpolateLightProb(x = shadetolerance, x0 = lowShadetol, x1 = highShadetol,
+                                                y0 = lowProb, y1 = highProb)]
+    newCohortData[, lightProb := tempDT$lightProb]
+  } else {
+    ## are there any decimals in shade tolerance trait values?
+    if (!all(newCohortData$shadetolerance == round(newCohortData$shadetolerance)))
+      stop("Species shade tolerance values (in sim$species) have decimals,
+           but interpolation of germination probabilities between
+           shade tolerance categories in sim$sufficientLight is FALSE. \n
+           Set interpolation to TRUE, or provide integer sim$species$shadetolerance values")
+
+    newCohortData[, lightProb := sufficientLight[cbind(shadetolerance, siteShade + 2)]]
+  }
 }
+
+#' Find interpolated value of light probability
+#'
+#' @param x the species shade tolerance trait value for which we want to find
+#'   the interpolated probability.
+#'
+#' @param x0 the \code{floor} of \code{x} corresponding to a class of shade
+#'   tolerance in the \code{sufficientLight} table.
+#'
+#' @param x1 the \code{ceiling} of \code{x} corresponding to a class of shade
+#'   tolerance in the \code{sufficientLight} table.
+#'
+#' @param y0 the probability of germination in the \code{sufficientLight} table
+#'   corresponding to \code{x0}.
+#'
+#' @param y1 the probability of germination in the \code{sufficientLight} table
+#'   corresponding to \code{x1}.
+#'
+#' @return  \code{vector} of the interpolated value
+
+.interpolateLightProb <- function(x, x0, x1, y0, y1) {
+  ## if floor and ceiling are the same and equal to x, shade tolerances (x) are integers
+  ## and the probability does not need to be interpolated
+  if (all((x1 - x0) == 0) & all(x == x1)) {
+    y <- y0
+  } else {
+    y <- ((y1 - y0)/(x1 - x0)) * (x - x0) + y0
+    y[is.nan(y)] <- y0[is.nan(y)]  ## as before, when shade tol. is an integer
+  }
+  y
+}
+
 
 #' Convert numeric values to rounded integers
 #'
