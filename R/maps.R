0fc13cbc0e27c35e7189f400617ea7fb29829296
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-09-23 20:40:52 -0700

    remove "no data" from speciesLayers

--- R/maps.R
+++ R/maps.R
@@ -687,6 +687,14 @@ loadkNNSpeciesLayers <- function(dPath, rasterToMatch, studyArea, sppEquiv,
   })
   names(speciesLayers) <- unique(kNNnames) ## TODO: see #10
 
+  # remove "no data" first
+  noData <- sapply(speciesLayers, function(xx) is.na(maxValue(xx)))
+  if (any(noData)) {
+    message(names(noData)[noData], " has no data in this study area; omitting it")
+    speciesLayers <- speciesLayers[!noData]
+  }
+
+  # remove "little data" first
   layersWdata <- sapply(speciesLayers, function(xx) if (maxValue(xx) < thresh) FALSE else TRUE)
   if (sum(!layersWdata) > 0) {
     sppKeep <- names(speciesLayers)[layersWdata]
