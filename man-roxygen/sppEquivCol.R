0ab5b2fd7cf640d42cc651a78d00ea638327d437
Eliot McIntire <eliotmcintire@gmail.com>
2021-01-20 13:37:03 -0800

    update doc content for sppEquiv and sppEquivCol that were for a different function

--- man-roxygen/sppEquivCol.R
+++ man-roxygen/sppEquivCol.R
@@ -1,3 +1,4 @@
 #' @param sppEquivCol the column name to use from \code{sppEquiv}.
-#'              Only necessary if \code{mixedType == 2}.
-#'              If not provided and \code{mixedType == 2}, will attempt to use "Boreal".
+#'     For functions that have \code{mixedType} e.g., \code{vegTypeMapGenerator},
+#'     Only necessary if \code{mixedType == 2}.
+#'     If not provided and \code{mixedType == 2}, will attempt to use "Boreal".
