dfb588fd359ddea002b20d5a3b43338d21ef42e6
Eliot McIntire <eliotmcintire@gmail.com>
2022-04-11 15:34:01 -0700

    with some documentation for sppHarmonize

--- R/sppHarmonize.R
+++ R/sppHarmonize.R
@@ -1,3 +1,19 @@
+#' Harmonize the 3 components that bring species into Biomass_**
+#'
+#' This function will attempt to harmonize many potential issues/conflicts that
+#' may arise under different combinations of supplied objects to the three
+#' arguments. See manual for details.
+#'
+#' @param sppEquiv A data.table or NULL representing the sppEquiv table (see manual).
+#'   If NULL, then it will use the `LandR::sppEquivalencies_CA`
+#' @param sppNameVector A character vector of species to use. These species must all
+#'   be from one naming convention, i.e., from one column in the sppEquiv.
+#' @param sppEquivCol A character string normally provided from the P(sim)$sppEquivCol
+#'   (see manual). If `NA`, the default, then this will try to determine which
+#'   column the `sppNameVector` used and use that. If `sppNameVector` is NULL, then
+#'   it will default to `"Boreal"`.
+#' @return Returns a named list with the same names as the arguments. These should
+#'   likely be assigned to the `sim` object in the module following this function call.
 sppHarmonize <- function(sppEquiv, sppNameVector, sppEquivCol) {
   if (is.null(sppEquiv) && is.null(sppNameVector)) {
     sppNameConvention <- "KNN"
