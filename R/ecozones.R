b573d3e4e35cc362c5033d6c6de495b40c1b46eb
Alex Chubaty <achubaty@for-cast.ca>
2022-11-17 20:10:28 -0700

    new function prepEcozonesRst()

---
+++ R/ecozones.R
@@ -0,0 +1,66 @@
+#' Prepare ecozones raster
+#'
+#' Download, rasterize, crop, mask, and reproject Canadian national ecozones shapefile.
+#'
+#' @param url character. ecozones shapefile url.
+#'            Default: <http://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip>.
+#'
+#' @template destinationPath
+#'
+#' @template studyArea
+#'
+#' @template rasterToMatch
+#'
+#' @return `RasterLayer`
+#'
+#' @export
+#' @importFrom fasterize fasterize
+#' @importFrom reproducible prepInputs
+#' @importFrom sf as_Spatial
+#' @importFrom sp proj4string
+prepEcozonesRst <- function(url, destinationPath, studyArea = NULL, rasterToMatch = NULL) {
+  if (!requireNamespace("terra", quietly = TRUE)) {
+    ## since terra is dependency of raster, it should already be installed, but just in case...
+    stop("Suggested package 'terra' not installed.\n",
+         "Install it using `install.packages('terra')`.")
+  }
+
+  if (is.null(url)) {
+    url <- "http://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip"
+  }
+
+  targetCRS <- NULL
+
+  if (!is.null(rasterToMatch)) {
+    targetCRS <- proj4string(rasterToMatch)
+  }
+
+  if (!is.null(studyArea)) {
+    studyArea <- as_Spatial(studyArea)
+    targetCRS <- proj4string(studyArea)
+  }
+
+  ecozone_shp <- prepInputs(
+    url = url,
+    targetFile = "ecozones.shp",
+    alsoExtract = "similar",
+    fun = "sf::st_read",
+    destinationPath = destinationPath,
+    studyArea = studyArea,
+    targetCRS = targetCRS
+  )
+
+  ecozone_shp[["ZONE_NAME"]] <- as.factor(ecozone_shp[["ZONE_NAME"]])
+  ecozone <- fasterize::fasterize(ecozone_shp, raster(rasterToMatch), field = "ZONE_NAME", fun = "sum")
+  ecozone <- terra::rast(ecozone)
+  ecozone <- terra::as.int(ecozone)
+
+  if (is(rasterToMatch, "Raster")) {
+    rasterToMatch <- terra::rast(rasterToMatch)
+  }
+
+  ecozone <- terra::project(ecozone, rasterToMatch, method = "near")
+  ecozone <- terra::mask(ecozone, rasterToMatch)
+
+  return(ecozone)
+}
