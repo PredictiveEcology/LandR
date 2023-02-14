d55d97819eedf072ee779f59680aa9f6e136eb4a
CeresBarros <cbarros@mail.ubc.ca>
2023-02-14 15:18:50 -0800

    terra(o)rize ecoregionProducer

--- R/ecoregions.R
+++ R/ecoregions.R
@@ -31,9 +31,9 @@ ecoregionProducer <- function(ecoregionMaps, ecoregionName = NULL, rasterToMatch
 
   # Alternative
   rstEcoregion <- list()
-  rtmNAs <- is.na(rasterToMatch[]) | rasterToMatch[] == 0
+  rtmNAs <- is.na(as.vector(rasterToMatch[])) | as.vector(rasterToMatch[]) == 0
   for (erm in seq(ecoregionMaps)) {
-    if (!is(ecoregionMaps[[erm]], "Raster")) {
+    if (!inherits(ecoregionMaps[[erm]], c("Raster", "SpatRaster"))) {
       message("ecoregionProducer fastRasterize: ", Sys.time())
       rstEcoregion[[erm]] <- fasterize::fasterize(sf::st_as_sf(ecoregionMaps[[erm]]),
                                                   raster(rasterToMatch),
@@ -43,27 +43,28 @@ ecoregionProducer <- function(ecoregionMaps, ecoregionName = NULL, rasterToMatch
     }
     rstEcoregion[[erm]][rtmNAs] <- NA
   }
-  rstEcoregionNAs <- do.call(`|`, lapply(rstEcoregion, function(x) is.na(x[]) | x[] == 0))
+  rstEcoregionNAs <- do.call(`|`, lapply(rstEcoregion, function(x) is.na(as.vector(x[])) | as.vector(x[]) == 0))
   NAs <- rtmNAs | rstEcoregionNAs
-  a <- lapply(rstEcoregion, function(x) getValues(x)[!NAs])
+  a <- lapply(rstEcoregion, function(x) as.vector(x[])[!NAs])
   b <- as.data.table(a)
   b[, (names(b)) := lapply(.SD, function(x) paddedFloatToChar(x, max(nchar(x), na.rm = TRUE)))]
 
   # Take the first 2 columns, whatever their names, in case they are given something
   ecoregionValues <- factor(paste(b[[1]], b[[2]], sep = "_"))
 
-  rstEcoregion <- raster(rstEcoregion[[1]])
+  rstEcoregion <- rasterRead(rstEcoregion[[1]])
   ecoregionFactorLevels <- levels(ecoregionValues)
 
   rstEcoregion[!NAs] <- as.integer(ecoregionValues)
-  levels(rstEcoregion) <- data.frame(ID = seq(ecoregionFactorLevels),
+  levs <- data.frame(ID = seq(ecoregionFactorLevels),
                                      mapcode = seq(ecoregionFactorLevels),
                                      ecoregion = gsub("_.*", "", ecoregionFactorLevels),
                                      landcover = gsub(".*_", "", ecoregionFactorLevels),
                                      ecoregion_lcc = ecoregionFactorLevels,
                                      stringsAsFactors = TRUE)
+  levels(rstEcoregion) <- levs
 
-  ecoregionTable <- as.data.table(raster::levels(rstEcoregion)[[1]])
+  ecoregionTable <- as.data.table(levs)
   message("ecoregionProducer mapvalues: ", Sys.time())
   ecoregionTable <- ecoregionTable[,.(active = "yes", mapcode, ecoregion, landcover, ecoregion_lcc)]
 
