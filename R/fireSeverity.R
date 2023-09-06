2ccd986ae38f648197dde5cfb177662c1e566ae1
CeresBarros <ceres.barros@gov.bc.ca>
2023-09-05 18:19:33 -0700

    bugfix calcFireSeverityB: export missing column

--- R/fireSeverity.R
+++ R/fireSeverity.R
@@ -45,5 +45,5 @@ calcSeverityB <- function(cohortData, burnedPixelCohortData) {
   severityData[, severityPropB := severityB/prefireB]
 
   ## drop prefireB and postfireB columns
-  return(severityData[, .(pixelGroup, pixelIndex, severityB)])
+  return(severityData[, .(pixelGroup, pixelIndex, severityB, severityPropB)])
 }
