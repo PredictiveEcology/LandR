1cd03deba23d60bef21aa3a80329bc13632c0c40
Jonathan Van Elslander <jonnyvan1@gmail.com>
2025-04-04 12:07:25 -0700

    NTEMS speciesInStudyArea
    Added new speciesPresentfromNTEMS function to create factor raster of species present across Canada based on NTEMS dominant species layer. Thus updated speciesInStudyArea to allow for option to use KNN species data or new NTEMS layer.

--- R/speciesPresentFromKNN.R
+++ R/speciesPresentFromKNN.R
@@ -1,5 +1,5 @@
 utils::globalVariables(c(
-  "allPres", "allPresFac", "KNN", "pixel", "variable"
+  "allPres", "allPresFac", "KNN", "pixel", "variable", "NTEMS_Species_Code"
 ))
 
 maskTo <- utils::getFromNamespace("maskTo", "reproducible")
@@ -38,7 +38,7 @@ projectTo <- utils::getFromNamespace("projectTo", "reproducible")
 #'   out <- googledrive::drive_put(zipFn)
 #'
 #'   ## Note: previous file (cropped to boreal forest)
-#'   ## available ot "1Oj78jJBeha5L6XDBBdWDAfimgNjYc9UD"
+#'   ## available at "1Oj78jJBeha5L6XDBBdWDAfimgNjYc9UD"
 #'   driveID <- "1J8fN7clZeqjd7yhiDWi13uoCBL8OensF"
 #'
 #'   ## Get species list
@@ -69,8 +69,10 @@ speciesPresentFromKNN <- function(year = 2011, dPath = asPath("."), res = 2000, 
   )
 
   templateCRS <- reproducible::prepInputs(
-    url = paste0("https://www12.statcan.gc.ca/census-recensement/2021/",
-                 "geo/sip-pis/boundary-limites/files-fichiers/lpr_000a21a_e.zip"),
+    url = paste0(
+      "https://www12.statcan.gc.ca/census-recensement/2021/",
+      "geo/sip-pis/boundary-limites/files-fichiers/lpr_000a21a_e.zip"
+    ),
     destinationPath = dPath
   )
   sa <- projectTo(studyAreaER, terra::crs(templateCRS))
@@ -103,6 +105,143 @@ speciesPresentFromKNN <- function(year = 2011, dPath = asPath("."), res = 2000, 
   return(c(speciesPres, numSp))
 }
 
+#' Make a species factor raster based on NTEMS Data
+#'
+#' This will download NTEMS dominant species layer for 2011 for forests of Canada, and make
+#' a factor raster at resolution provided by `res` (larger is faster).
+#'
+#' @param dPath A character string indicating where to download all the KNN layers
+#'
+#' @param res The resolution (one dimension, in m) for the resulting raster
+#'
+#' @param year Default (and only implemented) is 2011. This will download the 2011 NTEMS species layer
+#'
+#' @template rasterToMatch
+#'
+#' @template studyArea
+#'
+#' @param ... Additional arguments passed to [reproducible::Cache()]
+#'            and [equivalentName()]. Also valid: `outputPath`, and `studyAreaName`.
+#'
+#' @return A `SpatRaster` object with 2 layers: `"speciesPresent"` is a factor, with
+#' a legend (i.e., it is numbers on a map, that correspond to a legend) and
+#' `"numberSpecies"` which represents the number of species in each pixel.
+#'
+#' @examples
+#' \dontrun{
+#' if (requireNamespace("googledrive", quietly = TRUE)) {
+#'   # Make the dataset
+#'   speciesPresent <- speciesPresentFromNTEMS(dPath = "~/data/NTEMS")
+#'
+#'   # To upload this:
+#'   speciesPresentRas <- terra::rast(speciesPresent)[[1]]
+#'   fn <- "SpeciesPresentInCanadianForests_NTEMS.tif"
+#'   writeRaster(speciesPresentRas, file = fn)
+#'   zipFn <- gsub(".tif", ".zip", fn)
+#'   zip(files = dir(pattern = fn), zipFn)
+#'   out <- googledrive::drive_put(zipFn)
+#'
+#'   ## Get species list
+#'   sa <- LandR::randomStudyArea(size = 1e11)
+#'   species <- LandR::speciesInStudyArea(sa, dataSource = "NTEMS")
+#' }
+#' }
+#'
+#' @export
+#' @rdname speciesPresent
+speciesPresentFromNTEMS <- function(dPath = asPath("."), res = 1500, year = 2011,
+                                    rasterToMatch = NULL, studyArea = NULL, ...) {
+  dots <- list(...)
+
+  if (!requireNamespace("terra", quietly = TRUE)) {
+    ## since terra is dependency of raster, it should already be installed, but just in case...
+    stop(
+      "Suggested package 'terra' not installed.\n",
+      "Install it using `install.packages('terra')`."
+    )
+  }
+  oPath <- if (!is.null(dots$outputPath)) dots$outputPath else dPath
+
+  if ("cachePath" %in% names(dots)) {
+    cachePath <- dots$cachePath
+  } else {
+    cachePath <- getOption("reproducible.cachePath")
+  }
+
+  sppEquiv <- get(data("sppEquivalencies_CA",
+    package = "LandR",
+    envir = environment()
+  ), inherits = FALSE)
+
+  opts <- options("reproducible.useTerra" = TRUE)
+  on.exit(options(opts), add = TRUE)
+  studyAreaED <- Cache(
+    prepInputs,
+    fun = "terra::vect",
+    url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/district/ecodistrict_shp.zip",
+    destinationPath = dPath,
+    # fun = quote(SA_ERIntersect(x = targetFilePath, studyArea)),
+    overwrite = FALSE
+  )
+
+  templateCRS <- reproducible::prepInputs(
+    url = paste0(
+      "https://www12.statcan.gc.ca/census-recensement/2021/",
+      "geo/sip-pis/boundary-limites/files-fichiers/lpr_000a21a_e.zip"
+    ),
+    destinationPath = dPath
+  )
+  sa <- projectTo(studyAreaED, terra::crs(templateCRS))
+  sa <- vect(st_transform(st_as_sf(studyAreaED), crs = crs(templateCRS))) ################################### #stopgap solution to projectTo issues
+
+  SppURL <- paste0("https://opendata.nfis.org/downloads/forest_change/CA_Tree_Species_Classification_", year, ".zip")
+  SppTF <- paste0("Canada_Tree_Species_Classification_HMM_", year, ".tif")
+  SppRast <- prepInputs(
+    url = SppURL, targetFile = SppTF,
+    destinationPath = dPath
+  )
+
+  SppRast <- Cache(postProcess(SppRast, cropTo = sa, maskTo = sa))
+
+  sppEquiv <- sppEquiv[, .SD, .SDcol = c("NTEMS_Species_Code", "LandR")] # matching NTEMS spp code to sppEquivCol
+
+  uniqueVals <- as.data.table(terra::unique(SppRast))
+  setnames(uniqueVals, new = "NTEMS_Species_Code")
+  uniqueVals <- sppEquiv[uniqueVals, on = c("NTEMS_Species_Code")] # pulling all species from NTEMS layer
+  uniqueVals <- na.omit(uniqueVals) # removing non-treed areas
+  uniqueVals <- uniqueVals[, .(.N), by = .(NTEMS_Species_Code, LandR)] # grouping by species to eliminate duplicates caused by multiple subspecies in sppEquivalencies_CA
+  uniqueVals <- uniqueVals[, .(NTEMS_Species_Code, LandR)]
+
+  SppRast <- Cache(terra::segregate(SppRast, keep = TRUE))
+  SppRast <- Cache(terra::aggregate(SppRast, res / 30, fun = "modal"))
+  SppRast <- terra::subset(SppRast, "0", negate = TRUE)
+
+  names(SppRast) <- c(uniqueVals$LandR)
+
+  numSp <- sum(SppRast > 0)
+
+  mat <- terra::values(SppRast)
+  dt <- as.data.table(mat)
+  dt[, pixel := seq_len(.N)]
+  dt2 <- melt(dt, measure.vars = setdiff(colnames(dt), "pixel"), na.rm = TRUE, id.vars = "pixel")
+  dt2 <- dt2[value != 0]
+  setorderv(dt2, c("pixel", "variable"))
+  dt3 <- dt2[, list(allPres = paste(variable, collapse = "__")), by = "pixel"]
+  dt3[, allPresFac := factor(allPres)]
+
+  # Create a new empty rast
+  speciesPres <- terra::rast(SppRast[[1]])
+  # fill it with the integer values
+  speciesPres[dt3$pixel] <- as.integer(dt3$allPresFac)
+  names <- unique(dt3$allPresFac)
+  numerics <- as.integer(names)
+  # assign the levels
+  levels(speciesPres) <- data.frame(ID = numerics, category = names)
+
+
+  return(c(speciesPres, numSp))
+}
+
 #' Get species list in a given study area for a forest in Canada
 #'
 #' `speciesInStudyArea` defaults to use a url of a dataset uploaded to Google Drive that is
@@ -120,6 +259,10 @@ speciesPresentFromKNN <- function(year = 2011, dPath = asPath("."), res = 2000, 
 #' @param sppEquivCol An optional column from `LandR::sppEquivalencies_CA`.
 #'   If passed the KNN species will be returned according to this naming convention.
 #'
+#' @param dataSource Character. Either KNN or NTEMS. Defaults to KNN to obtain species from layer
+#'   created using species cover data from KNN. Also able to obtain species from NTEMS
+#'   Dominant Species Layer (though less species will likely be included).
+#'
 #' @param dPath Passed to `destinationPath` in `preProcess`.
 #'
 #' @return A named list of length 2: `speciesRas` is a factor `RasterLayer`
@@ -128,37 +271,72 @@ speciesPresentFromKNN <- function(year = 2011, dPath = asPath("."), res = 2000, 
 #'
 #' @export
 speciesInStudyArea <- function(studyArea, url = NULL, speciesPresentRas = NULL, sppEquivCol = NULL,
+                               dataSource = "KNN",
                                dPath = getOption("reproducible.destinationPath")) {
-  if (is.null(speciesPresentRas)) {
-    if (is.null(url)) {
-      url <- "https://drive.google.com/file/d/1J8fN7clZeqjd7yhiDWi13uoCBL8OensF"
+  if (!(dataSource %in% c("KNN", "NTEMS"))) {
+    stop("Data Source must be either KNN or NTEMS")
+  }
+  if (dataSource == "KNN") {
+    if (is.null(speciesPresentRas)) {
+      if (is.null(url)) {
+        url <- "https://drive.google.com/file/d/1J8fN7clZeqjd7yhiDWi13uoCBL8OensF"
+      }
+      speciesPres <- preProcess(url = url, destinationPath = dPath)
+      speciesPresRas <- rasterRead(speciesPres$targetFilePath)
+    } else {
+      speciesPresRas <- speciesPresentRas
     }
-    speciesPres <- preProcess(url = url, destinationPath = dPath)
-    speciesPresRas <- rasterRead(speciesPres$targetFilePath)
-  } else {
-    speciesPresRas <- speciesPresentRas
+
+    bb <- postProcess(x = speciesPresRas, studyArea = studyArea)
+
+    rasLevs <- as.data.table(levels(bb))
+    # if (is(speciesPresRas, "RasterLayer")) {
+    #   bb <- raster::deratify(bb)
+    # }
+
+    speciesCommunities <- na.omit(rasLevs[ID %in% as.vector(bb[[1]])]$category)
+    species <- as.character(speciesCommunities)
+    species <- unique(unlist(strsplit(species, "__")))
+
+    if (!is.null(sppEquivCol) & is.null(speciesPresentRas)) {
+      sppEquiv <- LandR::sppEquivalencies_CA
+      species <- unique(sppEquiv[KNN %in% species, .SD, ][[sppEquivCol]])
+      species <- species[!species == ""]
+    }
   }
+  if (dataSource == "NTEMS") {
+    warning("the NTEMS data includes only dominant species and thus using this dataset will likely result in fewer species returned")
+    if (is.null(speciesPresentRas)) {
+      if (is.null(url)) {
+        url <- "https://drive.google.com/file/d/1AytH5-4FZ3XpOFOiVFqUCLrMtavNmHuj"
+      }
+      speciesPres <- preProcess(url = url, destinationPath = dPath)
+      speciesPresRas <- rasterRead(speciesPres$targetFilePath)
+    } else {
+      speciesPresRas <- speciesPresentRas
+    }
 
-  bb <- postProcess(x = speciesPresRas, studyArea = studyArea)
+    bb <- postProcess(x = speciesPresRas, studyArea = studyArea)
 
-  rasLevs <- as.data.table(levels(bb))
-  # if (is(speciesPresRas, "RasterLayer")) {
-  #   bb <- raster::deratify(bb)
-  # }
+    rasLevs <- as.data.table(levels(bb))
+    # if (is(speciesPresRas, "RasterLayer")) {
+    #   bb <- raster::deratify(bb)
+    # }
 
-  speciesCommunities <- na.omit(rasLevs[ID %in% as.vector(bb[[1]])]$category)
-  species <- as.character(speciesCommunities)
-  species <- unique(unlist(strsplit(species, "__")))
+    speciesCommunities <- na.omit(rasLevs[value %in% as.vector(bb[[1]])]$category)
+    species <- as.character(speciesCommunities)
+    species <- unique(unlist(strsplit(species, "__")))
 
-  if (!is.null(sppEquivCol) & is.null(speciesPresentRas)) {
-    sppEquiv <- LandR::sppEquivalencies_CA
-    species <- unique(sppEquiv[KNN %in% species, .SD, ][[sppEquivCol]] )
-    species <- species[!species == ""]
+    if (!is.null(sppEquivCol) & is.null(speciesPresentRas)) {
+      sppEquiv <- LandR::sppEquivalencies_CA
+      species <- unique(sppEquiv[KNN %in% species, .SD, ][[sppEquivCol]])
+      species <- species[!species == ""]
+    }
   }
-
 
   return(list(speciesRas = bb, speciesList = species))
 }
+
 
 #' @keywords internal
 forestOutline <- function(x) {
@@ -188,5 +366,5 @@ SA_ERIntersect <- function(x, studyArea) {
 loadAndAggregateKNN <- function(dPath, res, sa) {
   ll <- loadkNNSpeciesLayers(dPath, sppEquiv = LandR::sppEquivalencies_CA, sppEquivCol = "KNN")
   llCoarse <- terra::aggregate(ll, res / 250)
-  postProcessTo(from = llCoarse, to = sa, method = "near")
+  postProcess(llCoarse, cropTo = sa, maskTo = sa, method = "near")
 }
