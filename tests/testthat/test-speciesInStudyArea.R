2e5585bb26b075d5f0a51bbf6c7577194fb376ca
Alex Chubaty <achubaty@for-cast.ca>
2025-10-07 15:17:59 -0600

    fix test with prev

--- tests/testthat/test-speciesInStudyArea.R
+++ tests/testthat/test-speciesInStudyArea.R
@@ -9,17 +9,18 @@ testthat::test_that("speciesInStudyArea works", {
   td <- withr::local_tempdir("dest_")
 
   targetCRS <- paste(
-    "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0",
-    "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+    "+proj=lcc +lat_0=0 +lon_0=-95 +lat_1=49 +lat_2=77",
+    "+x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
   )
   ecod <- reproducible::prepInputs(
     url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/district/ecodistrict_shp.zip",
-    destinationPath = td, fun = "sf::st_read"
+    destinationPath = td,
+    fun = "sf::st_read"
   )
   ecod <- ecod[ecod$ECODISTRIC == "1008", ]
   ecod <- sf::st_transform(ecod, targetCRS)
 
-  speciesInStudy <- speciesInStudyArea(ecod, dPath = td)
+  speciesInStudy <- speciesInStudyArea(ecod, dataSource = "KNN", dPath = td)
 
   testthat::expect_true("Pinu_Con" %in% speciesInStudy$speciesList)
   testthat::expect_false("Abie_Ama" %in% speciesInStudy$speciesList)
@@ -31,7 +32,11 @@ testthat::test_that("speciesInStudyArea works", {
 
   testthat::expect_warning(speciesInStudyArea(ecod, dPath = td, dataSource = "NTEMS"))
 
-  speciesInStudyNTEMS <- suppressWarnings(speciesInStudyArea(ecod, dPath = td, dataSource = "NTEMS"))
+  speciesInStudyNTEMS <- suppressWarnings(speciesInStudyArea(
+    ecod,
+    dPath = td,
+    dataSource = "NTEMS"
+  ))
 
   testthat::expect_true("Pinu_con" %in% speciesInStudyNTEMS$speciesList)
   testthat::expect_true("Abie_las" %in% speciesInStudyNTEMS$speciesList)
