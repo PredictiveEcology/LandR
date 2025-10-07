ee655e834911ee768acae66427b7d11291c02cec
Alex Chubaty <achubaty@for-cast.ca>
2025-10-06 20:26:58 -0600

    add documentation and cleanup

--- tests/testthat/test-cohorts.R
+++ tests/testthat/test-cohorts.R
@@ -1,36 +1,21 @@
 testthat::test_that("adjustAgeToLongevity works", {
-  # test that it returns expected results
-  cd <- data.table(
-    speciesCode = c("A", "A", "A", "B", "B"),
-    age = c(10, 200, 300, 200, 100)
-  )
-  traits <- data.table(
-    speciesCode = c("A", "B"),
-    longevity = c(200, 250)
-  )
+  ## test that it returns expected results
+  cd <- data.table(speciesCode = c("A", "A", "A", "B", "B"), age = c(10, 200, 300, 200, 100))
+  traits <- data.table(speciesCode = c("A", "B"), longevity = c(200, 250))
   expect_equal(
     suppressMessages(adjustAgeToLongevity(cd, traits, 0.9)),
-    data.table(
-      speciesCode = c("A", "A", "A", "B", "B"),
-      age = c(10, 177, 180, 200, 100)
-    )
+    data.table(speciesCode = c("A", "A", "A", "B", "B"), age = c(10, 177, 180, 200, 100))
   )
   expect_equal(
     suppressMessages(adjustAgeToLongevity(cd, traits, 0.7)),
-    data.table(
-      speciesCode = c("A", "A", "A", "B", "B"),
-      age = c(10, 140, 140, 173, 100)
-    )
+    data.table(speciesCode = c("A", "A", "A", "B", "B"), age = c(10, 140, 140, 173, 100))
   )
   expect_equal(
     suppressMessages(adjustAgeToLongevity(cd, traits, 0.5)),
-    data.table(
-      speciesCode = c("A", "A", "A", "B", "B"),
-      age = c(10, 100, 100, 125, 100)
-    )
+    data.table(speciesCode = c("A", "A", "A", "B", "B"), age = c(10, 100, 100, 125, 100))
   )
 
-  # test incorrect inputs
+  ## test incorrect inputs
   expect_error(adjustAgeToLongevity(cd, traits, 1.1))
   expect_error(adjustAgeToLongevity(cd, traits, NA))
   expect_error(adjustAgeToLongevity(cd, traits, 0.1))
