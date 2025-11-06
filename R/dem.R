872705cc15a3f3dc90a977091ee35e9ff5fd5f36
Alex Chubaty <achubaty@for-cast.ca>
2025-11-06 11:27:26 -0700

    dem.R cleanup / add pkg prefixes

--- R/dem.R
+++ R/dem.R
@@ -6,7 +6,7 @@
 #' @template rasterToMatch
 #' @template destinationPath
 #'
-#' @return `RasterLayer`
+#' @return `SpatRaster`
 #'
 #' @export
 prepInputsCanDEM <- function(studyArea, rasterToMatch, destinationPath) {
